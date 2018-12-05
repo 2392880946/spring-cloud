30f29375a08c5ea65d6c40025249222a3e6fa59d
2392880946 <2392880946qq.com>
2018-12-05 22:11:19 +0800

    更新

---
+++ springcloud-privoder-user/src/main/java/net/good/springcloud/contorller/TestBuloon.java
@@ -0,0 +1,13 @@
+package net.good.springcloud.contorller;
+
+import java.nio.charset.Charset;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+public class TestBuloon {
+
+    public static void main() {
+        int insertons = 100000000;
+    }
+}
